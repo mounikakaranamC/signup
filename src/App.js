a6eecfee517351408993c65b96ad274e16c6f02c
mounikakaranamC <mounikakaranam10@gmail.com>
2025-02-18 22:21:43 +0530

    package change

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Signin from './components/signin';
 import Signup from './components/signup';
-import Home from './components/home';
+// import Home from './components/home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Signin />} /> 
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/signin' element={<Signin/>}/>
       </Routes>
